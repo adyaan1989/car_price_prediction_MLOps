e24c6fea2aa123863fb927e7f910f290744995f2
kavyaan <adyaan.singh@gmail.com>
2025-10-04 00:18:55 +0530

    cicd updated

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,13 @@ FROM python:3.10
 WORKDIR /app
 
 COPY requirements.txt ./
+COPY setup.py ./
+COPY README.md ./
+COPY src/ ./src/
+
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-COPY . .
-
 EXPOSE 8080
-CMD ["python", "app.py"]
+
+CMD ["python", "src/car_sale_price_prediction/app.py"]
--- setup.py
+++ setup.py
@@ -1,30 +1,35 @@
 import setuptools
 
-with open("README.md", "r", encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__="0.0.0"
+__version__ = "0.0.0"
 
-REPO_NAME = "Car Sale Price Prediction"
-AUTHOR_USER_NAME = "Adyaan Singh"
-SRC_REPO = "Car Sale Price Prediction"
-AUTHOR_EMAIL ="adyaan.singh@gmail.com"
+REPO_NAME = "car-sale-price-prediction"
+AUTHOR_USER_NAME = "AdyaanSingh"
+SRC_REPO = "car_sale_price_prediction"
+AUTHOR_EMAIL = "adyaan.singh@gmail.com"
 
 setuptools.setup(
-    name = REPO_NAME,
+    name=REPO_NAME,
     version=__version__,
-    author = AUTHOR_USER_NAME,
-    description = "This project related to Car Sale Price Prediction Pipeline based on real time Dataset",
-    long_description = long_description,
-    long_description_content = "text/markdown",
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A project for Car Sale Price Prediction using real-time datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-
-    Project_urls = {
-        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-
-    package_dir={"":"src"},
-    packages=setuptools.find_packages(where="src")
-
-    
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    install_requires=[],  # Add your actual dependencies here or keep empty if using requirements.txt
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
